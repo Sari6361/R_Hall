9f2b916328b1a7515a40a048dce34eb2ca814389
sari Avramovich <s0556766361@gmail.com>
2023-12-03 00:50:35 +0200

    after hw

--- Solid.API/Program.cs
+++ Solid.API/Program.cs
@@ -1,3 +1,8 @@
+using Solid.Core.Repositories;
+using Solid.Core.Service;
+using Solid.Data.Repositories;
+using Solid.Service.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+ 
+builder.Services.AddSingleton<ICustomerRepository,CustomerRepository>();
+builder.Services.AddSingleton<ICateringRepository,CateringRepository>();
+builder.Services.AddSingleton<IEventRepoistory,EventRepository>();
+
+builder.Services.AddSingleton<ICustomerService, CustomerService>();
+builder.Services.AddSingleton<ICateringService, CateringService>();
+builder.Services.AddSingleton<IEventService, EventService>();
+
 
 var app = builder.Build();
 
